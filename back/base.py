4756100e4a5357b5b249f380b76395ccc97d79d7
m <mmailm@mail.ru>
2018-06-23 23:18:45 +0800

    fix

--- back/base.py
+++ back/base.py
@@ -1,8 +1,10 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/irkutsk2018'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', 'postgresql://postgres:123@localhost/irkutsk2018')
 
 db = SQLAlchemy(app)
