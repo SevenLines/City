0f252cba536b68b9fbd47027e88d43aa47f6203d
m <mmailm@mail.ru>
2018-06-26 00:33:07 +0800

    fix

--- fabfile.py
+++ fabfile.py
@@ -4,9 +4,9 @@ from invoke import task
 def deploy(c):
     with c.cd('~/projects/city'):
         c.run("git pull")
-        c.run("ls")
         with c.prefix(". /usr/local/bin/virtualenvwrapper.sh; workon city"):
             c.run("pip install -r requirements.txt")
         with c.cd("client"):
-            c.run("npm run install")
+            c.run("ls")
+            c.run("npm install")
             c.run("npm run build")
